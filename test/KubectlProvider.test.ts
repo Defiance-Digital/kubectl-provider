fe5fea19133f44baeeada129321360d0150f3c1b
Matthew Bonig <matthew.bonig@gmail.com>
2021-11-04 08:50:26 -0600

    I guess I'll just mock it

--- test/KubectlProvider.test.ts
+++ test/KubectlProvider.test.ts
@@ -1,24 +1,66 @@
 import { Template } from '@aws-cdk/assertions';
 import { Vpc } from '@aws-cdk/aws-ec2';
-import { App, Stack } from '@aws-cdk/core';
+import { Code, CodeConfig } from '@aws-cdk/aws-lambda';
+import { App, DockerImage, Stack } from '@aws-cdk/core';
 import { KubectlFunction } from '../src';
 
-test('Basic Lambda Snapshot', () => {
-  const app = new App();
-  const stack = new Stack(app, 'test');
+let fromAssetMock: any;
+let fromBuildMock: any;
 
-  const testVpc = Vpc.fromVpcAttributes(stack, 'TestVpc', {
-    privateSubnetIds: ['s-123'],
-    vpcId: 'vpc-123123',
-    availabilityZones: ['us-east-1a'],
+describe('LambdaFunction', () => {
+  beforeAll(() => {
+    // mock the Code calls so tests run quicker
+    fromAssetMock = jest.spyOn(Code, 'fromAsset').mockReturnValue({
+      isInline: false,
+      bind: (): CodeConfig => {
+        return {
+          s3Location: {
+            bucketName: 'my-bucket',
+            objectKey: 'my-key',
+          },
+        };
+      },
+      bindToResource: () => {
+        return;
+      },
+    } as any);
+    fromBuildMock = jest.spyOn(DockerImage, 'fromBuild').mockReturnValue({
+      isInline: false,
+      bind: (): CodeConfig => {
+        return {
+          s3Location: {
+            bucketName: 'my-bucket',
+            objectKey: 'my-key',
+          },
+        };
+      },
+      bindToResource: () => {
+        return;
+      },
+    } as any);
   });
-  new KubectlFunction(stack, 'TestFunction', {
-    roleArn: 'somerolearn',
-    clusterName: 'somecluster',
-    vpc: testVpc,
+  afterAll(() => {
+    // restore the Code from mock
+    fromAssetMock?.mockRestore();
+    fromBuildMock?.mockRestore();
   });
-  const assert = Template.fromStack(stack);
+  test('Basic Lambda Snapshot', () => {
+    const app = new App();
+    const stack = new Stack(app, 'test');
 
-  const resource = assert.findResources('AWS::Lambda::Function');
-  expect(resource).toMatchSnapshot();
+    const testVpc = Vpc.fromVpcAttributes(stack, 'TestVpc', {
+      privateSubnetIds: ['s-123'],
+      vpcId: 'vpc-123123',
+      availabilityZones: ['us-east-1a'],
+    });
+    new KubectlFunction(stack, 'TestFunction', {
+      roleArn: 'somerolearn',
+      clusterName: 'somecluster',
+      vpc: testVpc,
+    });
+    const assert = Template.fromStack(stack);
+
+    const resource = assert.findResources('AWS::Lambda::Function');
+    expect(resource).toMatchSnapshot();
+  });
 });
