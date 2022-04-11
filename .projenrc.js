04e2ee346535137a4f7ee26d69c54c312f73aa5d
Matthew Bonig <matthew.bonig@gmail.com>
2022-04-11 15:50:20 -0600

    lowercasing that name for reasons

--- .projenrc.js
+++ .projenrc.js
@@ -4,7 +4,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorAddress: 'matthew.bonig@defiance.ai',
   cdkVersion: '2.4.0',
   defaultReleaseBranch: 'main',
-  name: '@Defiance-Digital/kubectl-provider',
+  name: '@defiance-digital/kubectl-provider',
   repositoryUrl: 'https://github.com/Defiance-Digital/kubectl-provider.git',
   description: 'A Lambda function kubectl commands against an EKS repository',
   devDeps: ['eslint'],
