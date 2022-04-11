d34fe93b9e8dee6dc64a00fd19d4163a1d5d696c
Matthew Bonig <matthew.bonig@gmail.com>
2022-04-11 15:02:22 -0600

    updating integ test for

--- test/test.integ.ts
+++ test/test.integ.ts
@@ -4,9 +4,9 @@ import { KubectlFunction } from '../src';
 
 const app = new App();
 const stack = new Stack(app, 'TestingFunctionStack', { env: { account: '754664436462', region: 'us-east-1' } });
-const vpc = Vpc.fromLookup(stack, 'Vpc', { vpcId: 'vpc-0c7c5d99e5c4ab76a' });
+const vpc = Vpc.fromLookup(stack, 'Vpc', { vpcId: 'vpc-084519be7c1b8134c' });
 new KubectlFunction(stack, 'TestFunction', {
   vpc,
-  roleArn: 'arn:aws:iam::754664436462:role/qa1-Cluster-ClusterCreationRole360249B6-2MHDR3584NLC',
+  roleArn: 'arn:aws:iam::581514672367:role/qa1-Cluster-ClusterCreationRole360249B6-2MHDR3584NLC',
   clusterName: 'qa1',
 });
