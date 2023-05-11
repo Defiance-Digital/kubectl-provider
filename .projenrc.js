c50659301883fe48c666951144378dbd623307f6
Matthew Bonig <matthew.bonig@gmail.com>
2023-05-11 16:16:24 -0600

    updating versions and docs.

--- .projenrc.js
+++ .projenrc.js
@@ -5,6 +5,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   cdkVersion: '2.75.0',
   defaultReleaseBranch: 'main',
   depsUpgrade: false,
+  homepage: 'https://defiance.ai',
   name: '@defiance-digital/kubectl-provider',
   repositoryUrl: 'https://github.com/Defiance-Digital/kubectl-provider.git',
   description: 'A Lambda function kubectl commands against an EKS repository',
@@ -16,6 +17,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   integrationTestAutoDiscover: false,
   releaseToNpm: true,
   npmAccess: javascript.NpmAccess.PUBLIC,
+  jsiiVersion: '^5.0.7',
 });
 project.compileTask.exec('rm -rf lib/handlers/; mkdir lib/handlers/', { name: 'mkdir python handler' });
 project.compileTask.exec('cp src/handlers/*.py lib/handlers/', { name: 'copy python handler' });
